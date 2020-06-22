9e70f2b6d854643ae8a95c6911ed44c4461c5cb2
Taylor Sam <taylorsam209@yahoo.com>
2020-06-22 15:46:38 -0700

    update our menu-item with navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <HomePage />
+      </Switch>
     </div>
   );
 }
