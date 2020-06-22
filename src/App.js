bf6046ba75fada4db22eedc086b4424a3306757f
Taylor Sam <taylorsam209@yahoo.com>
2020-06-22 16:29:01 -0700

    added collection-item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component'
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
         <HomePage />
       </Switch>
     </div>
